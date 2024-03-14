8dc6dd1c401eff77d89615ca4f5c131604cf188c
Lexter T <lexter.tapawan@gmail.com>
2024-03-14 10:24:46 +0000

    Completed

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Lexter Tapawan on 13/03/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
